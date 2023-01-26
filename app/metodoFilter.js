5249b31db6709da947952df31d1494185c247aad
jobemcamera <jobecamera@gmail.com>
2023-01-26 19:26:33 -0300

    mostrando valor total dos livros disponíveis com metodo Reduce

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -5,6 +5,27 @@ function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value
     // livros vem do main.js que já possui todos os livros json
-    let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria)
+    let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade() : filtrarPorCategoria(categoria)
     exibirOsLivrosNaTela(aplicarDesconto(livrosFiltrados))
+    
+    if (categoria == 'disponivel') {
+        const valorTotal = calcularValorTotalDeLivrosDisponiveis(aplicarDesconto(livrosFiltrados))
+        exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal)
+    }
+}
+
+function filtrarPorCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria)
+}
+
+function filtrarPorDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0)
+}
+
+function exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal) {
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
+        <div class="livros__disponiveis">
+            <p>Todos os livros disponíveis por R$ <span id="valor">R$${valorTotal}</span></p>
+        </div>
+    `
 }
