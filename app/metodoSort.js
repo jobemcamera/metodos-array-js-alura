1f62a4b5e127ec87f9d5ecf209b0597423d997cb
jobemcamera <jobecamera@gmail.com>
2023-01-26 18:41:54 -0300

    ordenação de preço com o método sort

---
+++ app/metodoSort.js
@@ -0,0 +1,9 @@
+let btnOrdenarPorPreco = document.getElementById('btnOrdenarPorPreco')
+btnOrdenarPorPreco.addEventListener('click', ordenarLivrosPorPreco)
+
+function ordenarLivrosPorPreco() {
+    // sort((a, b) => a.preco - b.preco) ordena do menor para o maior
+    // sort((a, b) => b.preco - a.preco) ordena do maior para o menor
+    let livrosOrdenados = livros.sort((a, b) => a.preco - b.preco)
+    exibirOsLivrosNaTela(livrosOrdenados)
+}
